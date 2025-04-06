5c579f0dba4e1ea8f27ff275e339af82dc10d814
NguyenPhuocSang1695 <sangnguyen20050916@gmail.com>
2025-04-06 21:47:27 +0700

    sửa trang phân loại

--- src/js/phan-loai.js
+++ src/js/phan-loai.js
@@ -110,16 +110,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     card.className = "card mb-3";
     card.innerHTML = `
       <div class="card-body">
-        <h5 class="card-title">
-          <a href="user-sanpham.php?id=${
-            product.ProductID
-          }" class="text-decoration-none text-dark">
-            ${product.ProductName}
-          </a>
-        </h5>
-        <p class="card-text"><strong>Giá:</strong> ${Number(
-          product.Price
-        ).toLocaleString()} VNĐ</p>
         <a href="user-sanpham.php?id=${product.ProductID}">
           <img src="..${
             product.ImageURL
@@ -127,6 +117,16 @@ document.addEventListener("DOMContentLoaded", async function () {
       product.ProductName
     }">
         </a>
+        <h5 class="card-title">
+          <a href="user-sanpham.php?id=${
+            product.ProductID
+          }" class="text-decoration-none text-dark style="margin: 10px 0; font-weight: bold;">">
+            ${product.ProductName}
+          </a>
+        </h5>
+        <p class="card-text"><strong>Giá:</strong> ${Number(
+          product.Price
+        ).toLocaleString()} VNĐ</p>
       </div>
     `;
     return card;
