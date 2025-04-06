0f95ce2d9cf21e9ef073c18546c560f71683cc00
NguyenPhuocSang1695 <sangnguyen20050916@gmail.com>
2025-04-06 13:33:26 +0700

    thêm điều kiện lọc và tìm kiếm. điều kiện status = appear

--- php-api/filter-product.php
+++ php-api/filter-product.php
@@ -4,7 +4,7 @@ $conn = connect_db();
 if (isset($_GET['category_id'])) {
     $category_id = intval($_GET['category_id']);
 
-    $sql = "SELECT * FROM products WHERE CategoryID = ?";
+    $sql = "SELECT * FROM products WHERE CategoryID = ? AND Status = 'appear'";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $category_id);
     $stmt->execute();
